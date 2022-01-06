4fbd0d9f8235dda2324f9c44179378d44ce3546a
Christian <cristian117acosta@hotmail.com>
2022-01-06 15:20:27 -0600

    Pruebas

---
+++ 01-fundamentos/app.js
@@ -0,0 +1,12 @@
+// console.log(console.log('hola'));
+
+let   a = 10;
+var   b = 10;
+const c = 10;
+
+
+
+// ES5 -> Soportado en todos los navegadores web 
+// ES6/ES2015, ES7/ES2016, ES8/ES2017 -> soportados por la mayoría de navegadores modernos, pero no por todos los navegadores web
+
+Polyfill -> 
