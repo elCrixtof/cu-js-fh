ab661126862316e03994d25bbae10ceab703c6d1
Christian <cristian117acosta@hotmail.com>
2022-01-14 10:40:01 -0600

    .

--- 02-blackjack/assets/js/juego.js
+++ 02-blackjack/assets/js/juego.js
@@ -7,7 +7,15 @@
 
 let deck = [];
 const tipos = ['C', 'D', 'H', 'S'];
-const restante = ['A', 'J', 'Q', 'K']
+const restante = ['A', 'J', 'Q', 'K'];
+let puntosJugador = 0;
+let puntosComputadora = 0;
+
+// Referencias del HTML
+const btnNuevo = document.querySelector('#btnNuevo');
+const btnPedir = document.querySelector('#btnPedir');
+const btnDetener = document.querySelector('#btnDetener');
+const puntosHTML = document.querySelectorAll('small');
 
 // Esta función crea una nueva baraja
 const crearDeck = () => {
@@ -21,28 +29,23 @@ const crearDeck = () => {
             deck.push(element+tipo);
         }
     }
-
-    console.log(deck);
+    // la librería underscore contiene funciones especiales
+    // en este caso se usa para desacomodar los elementos 
+    // del arreglo de forma aleatoria
     deck = _.shuffle(deck);
-    console.log(deck);
     return deck;
 };
 
-crearDeck();
-
 // Esta función me permite devolver una carta random del deck
-const tomaCarta =  () => {
+const pedirCarta =  () => {
     if(deck.length === 0) {
         throw 'No hay cartas en el deck';
     }
     const card = deck.pop();
-    console.log(card);
     return card;
 };
 
-tomaCarta();
-console.log(deck);
-
+// Esta función cálcula el valor de la carta 
 const valorCarta = (card) => {
     let valor = card.substring(0, card.length - 1);
     // if( isNaN(valor) ) {
@@ -54,9 +57,15 @@ const valorCarta = (card) => {
     // console.log(valor); 
     valor = isNaN(valor) ? 
             valor = (valor === 'A') ? 11 : 10 :
-            valor * 1;
+            valor * 1; // Multiplicar una cadena por 1 es como castear el tipo de dato
     return valor;
 };
 
-const valor  = valorCarta(tomaCarta());
-console.log(valor);
+crearDeck();
+
+// Eventos
+btnPedir.addEventListener('click', function() {
+    const carta = pedirCarta();
+    puntosJugador += valorCarta(carta);
+    puntosHTML[0].innerText = puntosJugador;
+});
