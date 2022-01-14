cc1b1be9f57432105807c9b5d830172f6c4d48b5
Christian <cristian117acosta@hotmail.com>
2022-01-13 18:10:01 -0600

    .

--- 02-blackjack/assets/js/juego.js
+++ 02-blackjack/assets/js/juego.js
@@ -9,6 +9,7 @@ let deck = [];
 const tipos = ['C', 'D', 'H', 'S'];
 const restante = ['A', 'J', 'Q', 'K']
 
+// Esta función crea una nueva baraja
 const crearDeck = () => {
     for(let i = 2; i <= 10; i++) {
         for(let tipo of tipos){
@@ -20,7 +21,42 @@ const crearDeck = () => {
             deck.push(element+tipo);
         }
     }
+
+    console.log(deck);
+    deck = _.shuffle(deck);
+    console.log(deck);
+    return deck;
 };
 
 crearDeck();
+
+// Esta función me permite devolver una carta random del deck
+const tomaCarta =  () => {
+    if(deck.length === 0) {
+        throw 'No hay cartas en el deck';
+    }
+    const card = deck.pop();
+    console.log(card);
+    return card;
+};
+
+tomaCarta();
 console.log(deck);
+
+const valorCarta = (card) => {
+    let valor = card.substring(0, card.length - 1);
+    // if( isNaN(valor) ) {
+    //     valor = (valor === 'A') ? 11: console.log('No es un número');       
+    // } else {
+    //     console.log('Es un número');
+    //     valor = valor * 1;
+    // }
+    // console.log(valor); 
+    valor = isNaN(valor) ? 
+            valor = (valor === 'A') ? 11 : 10 :
+            valor * 1;
+    return valor;
+};
+
+const valor  = valorCarta(tomaCarta());
+console.log(valor);
