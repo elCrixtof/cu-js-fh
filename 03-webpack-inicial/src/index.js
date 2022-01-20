a1874860aaf7a2c6cc29aaa66ba0f59cbd2062c3
Christian <cristian117acosta@hotmail.com>
2022-01-20 17:09:50 -0600

    .

---
+++ 03-webpack-inicial/src/index.js
@@ -0,0 +1,5 @@
+import {saludar} from './js/componentes.js'
+    
+const nombre = 'Christian';
+
+saludar(nombre);
---
+++ 03-webpack-inicial/webpack.config.js
@@ -0,0 +1,30 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    output: {
+        clean: true
+    },
+
+    module: {
+        rules: [
+           {
+               test: /\.html$/,
+               loader: 'html-loader',
+               options: {
+                   sources: false
+               }
+           } 
+        ]
+    },
+
+    optimization: {},
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Mi Webpack App',
+            // filename: 'index.html'
+            template: './src/index.html'
+        })
+    ],
+}
