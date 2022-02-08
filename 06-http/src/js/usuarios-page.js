68731e0ed204dffa4a7e264db81a1745496be51f
Christian <cristian117acosta@hotmail.com>
2022-02-08 11:35:44 -0600

    git commit -m "Remove .DS_Store from everywhere"

---
+++ 06-http/src/js/usuarios-page.js
@@ -0,0 +1,77 @@
+
+
+
+const body  = document.body;
+
+const crearHtml = () => {
+    
+    const html = `
+    <h1 class="mt-5"> Usuarios</h1>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">email</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Avatar</th>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
+    </table>
+    `;
+
+    const div = document.createElement('div');
+    div.innerHTML = html;
+    body.appendChild( div );
+
+    // Crear una referencia al TBODY
+    // ya que los TRs van dentro del tbody
+            // querySelector('tbody');
+            // Crear una variable para mantener la referencia?
+
+}
+
+
+// La función crearFilaUsuario debería de recibir un UNICO usuario
+// con la siguiente estructura
+    // {
+    //     "id": 7,
+    //     "email": "michael.lawson@reqres.in",
+    //     "first_name": "Michael",
+    //     "last_name": "Lawson",
+    //     "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/follettkyle/128.jpg"
+    // }
+const crearFilaUsuario = ( usuario ) => {
+
+    // En la tabla deben de colocar un correlativo empezando en 1
+    // También deben de colocar el avatar
+
+    const html = `
+        <td scope="col"> 1. </td>
+        <td scope="col"> michael.lawson@reqres.in </td>
+        <td scope="col"> Michael Lawson </td>
+        <td scope="col">
+            <img class="img-thumbnail" src="">
+        </td>
+    `;
+
+    const tr = document.createElement('tr');
+    tr.innerHTML = html;
+
+    // Añadir el table row (tr) dentro del TBody creado anteriormente
+
+}
+
+
+export const init = async() => {
+
+    crearHtml();
+
+    // Obtener la lista de usuarios usando el servicio creado
+    // Por cada usuario, llamar la función crearFila (for, forEach)
+    // Colocar el init en el index.js, para que se ejecute la creación
+
+}
+
